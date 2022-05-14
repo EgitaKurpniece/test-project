551a3bc283441459292e921274542d29a7faa94d
EgitaKurpniece <egita.kurpniece@gmail.com>
2022-05-14 15:43:49 +0000

    Add page controller items using the index method

---
+++ resources/views/page.blade.php
@@ -0,0 +1,6 @@
+Hello universe!
+
+This is the attempt at using index() method by {{ $name }}. 
+<br>
+<br>
+It took me many hours and a lot of online help, and I still don't think I get how it actually works.
